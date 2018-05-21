c206bc25ffad91b174f765447c3adaf9842d20a4
Ezra Keddell <ezrakeddell@gmail.com>
2018-05-21 13:00:28 +1200

    update ministry model values

--- models/audio.js
+++ models/audio.js
@@ -1,5 +1,12 @@
 module.exports = function(sequelize, DataTypes) {
   const Audio = sequelize.define('Audio', {
+    id: {
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV1,
+      primaryKey: true,
+      allowNull: false,
+      unique: true
+    },
     title: DataTypes.STRING,
     date: DataTypes.DATE,
     image: DataTypes.STRING,
--- models/ministry.js
+++ models/ministry.js
@@ -1,9 +1,18 @@
-'use strict';
 module.exports = function(sequelize, DataTypes) {
   const Ministry = sequelize.define('Ministry', {
+    id: {
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV1,
+      primaryKey: true,
+      allowNull: false,
+      unique: true
+    },
     title: DataTypes.STRING,
     image: DataTypes.STRING,
-    body: DataTypes.STRING
+    text: DataTypes.STRING,
+    updated: DataTypes.DATE,
+    status: DataTypes.STRING,
+    churches: DataTypes.ARRAY
   });
   return Ministry;
 };
